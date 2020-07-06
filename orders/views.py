1cbf76b183fc916e97e1155889620dbe3948ecc9
mostafaala2 <a_mona_24@hotmail.com>
2020-07-06 19:51:27 +0200

    Views & URLs

--- orders/views.py
+++ orders/views.py
@@ -2,5 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Project 3: TODO")
+
+def cart(request):
+	context = {}
+	return render(request, 'orders/cart.html', context)
+
+def checkout(request):
+    return render(request,'orders/checkout.html')
+
+def manus(request):
+
+    return render(request, "orders/manus.html")
